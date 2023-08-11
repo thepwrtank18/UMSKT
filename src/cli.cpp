13b297bbbf7ae87ba52d672366f7149fc4469cae
pottzman <pottzman@hotmail.com>
2023-08-11 17:24:36 +1000

    Update cli.cpp

--- src/cli.cpp
+++ src/cli.cpp
@@ -53,7 +53,7 @@ void CLI::showHelp(char *argv[]) {
     fmt::print("\t-n --number\tnumber of keys to generate (defaults to 1)\n");
     fmt::print("\t-f --file\tspecify which keys file to load\n");
     fmt::print("\t-i --instid\tinstallation ID used to generate confirmation ID\n");
-    fmt::print("\t-m --mode\tproduct family to activate. valid options are \"WINDOWS\", \"OFFICEXP\", \"OFFICE2K3\", \"OFFICE2K7\", \"PLUSDME\" (defaults to \"WINDOWS\")\n");
+    fmt::print("\t-m --mode\tproduct family to activate.\n\t\t\toptions are \"WINDOWS\", \"OFFICEXP\", \"OFFICE2K3\", \"OFFICE2K7\", \"PLUSDME\" (defaults to \"WINDOWS\")\n");
     fmt::print("\t-p --productid\tthe product ID of the Program to activate. only required for Office 2K3+ programs\n");
     fmt::print("\t-b --binkid\tspecify which BINK identifier to load (defaults to 2E)\n");
     fmt::print("\t-l --list\tshow which products/binks can be loaded\n");
