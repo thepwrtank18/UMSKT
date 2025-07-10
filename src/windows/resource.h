dba5e99233d6904ad61e656310de7a171dd95b5e
TheTank20 <pt18.relay@gmail.com>
2025-07-10 15:54:49 -0500

    resource stuff

--- src/windows/resource.h
+++ src/windows/resource.h
@@ -20,19 +20,9 @@
  * @Maintainer Neo
  */
 
-//{{NO_DEPENDENCIES}}
-// Microsoft Visual C++ generated include file.
-// Used by umskt.rc
-//
-#define IDI_ICON1                       101
+#ifndef UMSKT_RESOURCE_H
+#define UMSKT_RESOURCE_H
 
-// Next default values for new objects
-//
-#ifdef APSTUDIO_INVOKED
-#ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        102
-#define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1001
-#define _APS_NEXT_SYMED_VALUE           101
-#endif
-#endif
+// No resource IDs needed since we're using raw numbers in the .rc file
+
+#endif // UMSKT_RESOURCE_H
